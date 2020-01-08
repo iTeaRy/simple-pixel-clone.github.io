9e723680e69687be9d53629d2911495ed78199b3
OlegRudevich <olegrudevich19@gmail.com>
2020-01-08 22:15:22 +0300

    init: create web page

---
+++ precache-manifest.95d9136c8cd47d836f1ca324e7ef894e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "bc1c61d04619b44573af",
+    "url": "./static/js/main.714a86f8.chunk.js"
+  },
+  {
+    "revision": "6895de19234298545441",
+    "url": "./static/js/2.27f4a9df.chunk.js"
+  },
+  {
+    "revision": "bc1c61d04619b44573af",
+    "url": "./static/css/main.9e72f662.chunk.css"
+  },
+  {
+    "revision": "050fb6507c6b676e491809ee424b8303",
+    "url": "./index.html"
+  }
+];
